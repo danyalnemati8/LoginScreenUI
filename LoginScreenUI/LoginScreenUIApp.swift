13d3246817eeed2ee6534be813cad63accfaeae1
danyalnemati8 <70175019+danyalnemati8@users.noreply.github.com>
2023-07-19 12:53:38 -0700

    Initial Commit

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by Danyal Nemati on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
