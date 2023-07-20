ba846c2d9336d1e5518f076eff45287ddc3d8ae4
danyalnemati8 <70175019+danyalnemati8@users.noreply.github.com>
2023-07-19 18:16:39 -0700

    new functionality followed a tutorial

--- LoginScreenUI/ContentView.swift
+++ LoginScreenUI/ContentView.swift
@@ -18,11 +18,58 @@ struct ContentView: View {
         NavigationView {
             ZStack {
                 Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.45)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text:$username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .border(.red, width: CGFloat(wrongUsername))
+                    SecureField("Password", text:$password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .border(.red, width: CGFloat(wrongPassword))
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
                     
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
             }
-        
+            .navigationBarHidden(true)
         }
-        .navigationBarHidden(true)
+        
+    }
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "danyal"{
+            wrongUsername = 0
+            if password.lowercased() == "abc123" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
+            
     }
 }
 
